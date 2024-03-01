ce7b1c84493c773de61aede7333b7bae2bd864c1
Matsadura <zidanditoo1@gmail.com>
2024-03-01 12:07:12 +0100

    [UPDATE] Check list == NULL

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -34,7 +34,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current = *list, *next_node;
 
-	if (current == NULL || current->next == NULL)
+	if (current == NULL || current->next == NULL || !list)
 		return;
 	while (current != NULL)
 	{
