f793dde20749464b1d3e049d983610716c0fb5a7
Matsadura <zidanditoo1@gmail.com>
2024-03-01 12:31:44 +0100

    [UPDATE] ADD Node2 is head condition

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,7 +11,10 @@ void swap_node(listint_t **h, listint_t *node1, listint_t *node2)
 	if (node1 == NULL || node2 == NULL || node1 == node2)
 		return;
 
-	node2->prev = node1->prev;
+	if (node2->prev)
+		node2->prev = node1->prev;
+	else
+		*h = node1;
 	node1->next = node2->next;
 
 	if (node1->prev)
